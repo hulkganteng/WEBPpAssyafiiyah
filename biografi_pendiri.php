264002998794d688113248c8879445c4c3136a22
hulkganteng <bopupalukman@gmail.com>
2024-08-06 21:22:39 +0700

    updates3

--- biografi_pendiri.php
+++ biografi_pendiri.php
@@ -5,7 +5,7 @@
     <!-- Foto Pendiri -->
     <div class="row mb-4">
         <div class="col-md-4">
-            <img src="images/pendiri.jpg" class="img-fluid rounded" alt="Foto Pendiri">
+            <img src="./images/pendiri.jpg" class="img-fluid rounded" alt="Foto Pendiri">
         </div>
         <div class="col-md-8">
             <h1>Biografi Pendiri</h1>
--- includes/header.php
+++ includes/header.php
@@ -12,6 +12,45 @@
             height: 40px;
             margin-right: 10px;
         }
+        .slideshow-container {
+            position: relative;
+            max-width: 100%;
+            margin: auto;
+            overflow: hidden;
+        }
+        .mySlides {
+            display: none;
+        }
+        .fade {
+            animation-name: fade;
+            animation-duration: 1.5s;
+        }
+        @keyframes fade {
+            from {opacity: .4}
+            to {opacity: 1}
+        }
+        .text {
+            color: #f2f2f2;
+            font-size: 15px;
+            padding: 8px 12px;
+            position: absolute;
+            bottom: 8px;
+            width: 100%;
+            text-align: center;
+        }
+        .numbertext {
+            color: #f2f2f2;
+            font-size: 12px;
+            padding: 8px 12px;
+            position: absolute;
+            top: 0;
+        }
+
     </style>
 </head>
 <body>
+<script>
+    src="https://code.jquery.com/jquery-3.5.1.min.js"
+    src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"
+    src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+</script>
