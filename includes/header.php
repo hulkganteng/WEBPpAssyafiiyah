2a73becf450af5ac2973744bf261ce242ee1ce70
BHY <66734381+hulkganteng@users.noreply.github.com>
2024-08-09 22:14:40 +0700

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -20,6 +20,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="icon" href="/images/favicon.png" type="image/png">
     <style>
         .navbar-brand img {
             height: 40px;
